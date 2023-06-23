7e2b201d092b135a56dfdb7bdb8d3cfd181d5d90
RedHaider <syedaliredwanulhaider13@gmail.com>
2023-06-23 23:10:26 +0600

    first commit

---
+++ tracker/src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+/* eslint-disable vue/multi-word-component-names */
+
+export default {
+  name: 'AppHeader', // Renamed the component to "AppHeader"
+  props: {
+    title: String,
+  }
+}
+</script>
+
+<style scoped>
+  header{
+    display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+
+  }
+</style>
